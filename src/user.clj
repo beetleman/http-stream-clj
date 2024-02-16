047529e1c3babc13c7938b252c5627eb491950de
Mateusz Probachta <Mateusz.Probachta@vertexinc.com>
2024-02-16 10:42:32 +0100

    add stats

--- src/user.clj
+++ src/user.clj
@@ -1,7 +1,17 @@
 (ns user
   (:require [beetleman.http-stream-clj.server :as server]))
 
+
+(defn print-stats []
+  (let [runtime (Runtime/getRuntime)
+        print-stat (fn [name v]
+                     (println (format "%s: %.2fm" name (/ v 1024.0 1024.0)))) ]
+    (print-stat "Max memory" (.maxMemory runtime))
+    (print-stat "Free memory" (.freeMemory runtime))
+    (print-stat "Total memory" (.totalMemory runtime))))
+
 (defn start []
+  (print-stats)
   (server/start))
 
 (defn stop []
